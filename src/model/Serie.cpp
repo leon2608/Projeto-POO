760f12bc64c1621fd3639384dced198c28fbd8c7
Leonardo <l251773@dac.unicamp.br>
2024-05-13 14:43:54 -0300

    Implementação da classe Série

---
+++ src/model/Serie.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <string>
+
+#include "Serie.h"
+
+using namespace std;
+
+Serie::Serie(int internalId, string serieName, int releaseYear, int season,
+             int episodeCount, string mainActors, string mainCharacters,
+             string network, int rating)
+{
+    this->internalId = internalId;
+    this->serieName = serieName;
+    this->releaseYear = releaseYear;
+    this->season = season;
+    this->episodeCount = episodeCount;
+    this->mainActors = mainActors;
+    this->mainCharacters = mainCharacters;
+    this->network = network;
+    this->rating = rating;
+}
+Serie::~Serie()
+{
+}
+
+int Serie::getInternalId()
+{
+    return this->internalId;
+}
+
+string Serie::getSerieName()
+{
+    return this->serieName;
+}
+
+int Serie::getReleaseYear()
+{
+    return this->releaseYear;
+}
+
+int Serie::getSeason()
+{
+    return this->season;
+}
+
+int Serie::getEpisodeCount()
+{
+    return this->episodeCount;
+}
+
+string Serie::getMainActors()
+{
+    return this->mainActors;
+}
+
+string Serie::getMainCharacters()
+{
+    return this->mainCharacters;
+}
+
+string Serie::getNetwork()
+{
+    return this->network;
+}
+
+int Serie::getRating()
+{
+    return this->rating;
+}
+
+void Serie::setSerieName(string name)
+{
+    this->serieName = name;
+}
+
+void Serie::setReleaseYear(int year)
+{
+    this->releaseYear = year;
+}
+
+void Serie::setSeason(int season)
+{
+    this->season = season;
+}
+
+void Serie::setEpisodeCount(int espisodeNumber)
+{
+    this->episodeCount = espisodeNumber;
+}
+
+void Serie::setMainActors(string mainActors)
+{
+    this->mainActors = mainActors;
+}
+
+void Serie::setMainCharacters(string getMainCharacters)
+{
+    this->mainCharacters = mainCharacters;
+}
+
+void Serie::setNetwork(string network)
+{
+    this->network = network;
+}
+
+void Serie::setRating(int rating)
+{
+    this->rating = rating;
+}
--- src/model/Serie.h
+++ src/model/Serie.h
@@ -1,6 +1,46 @@
 #ifndef SERIE_H
 #define SERIE_H
 
-// TODO: Implement Serie class
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Serie
+{
+private:
+    int internalId;
+    string serieName;
+    int releaseYear;
+    int season;
+    int episodeCount;
+    string mainActors;
+    string mainCharacters;
+    string network;
+    int rating;
+
+public:
+    Serie(int, string, int, int, int, string, string, string, int);
+    virtual ~Serie();
+
+    int getInternalId();
+    string getSerieName();
+    int getReleaseYear();
+    int getSeason();
+    int getEpisodeCount();
+    string getMainActors();
+    string getMainCharacters();
+    string getNetwork();
+    int getRating();
+
+    void setSerieName(string name);
+    void setReleaseYear(int year);
+    void setSeason(int season);
+    void setEpisodeCount(int episodeNumber);
+    void setMainActors(string mainActors);
+    void setMainCharacters(string mainCharacters);
+    void setNetwork(string network);
+    void setRating(int rating);
+};
 
 #endif // SERIE_H
