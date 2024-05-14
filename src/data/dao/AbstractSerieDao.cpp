8d1686957674d7315be675f71c6f053628b272f6
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-05-13 22:10:14 -0300

    Criação da classe AbstractSerieDao

---
+++ src/data/dao/AbstractSerieDao.cpp
@@ -0,0 +1,5 @@
+#include "AbstractSerieDao.h"
+
+AbstractSerieDao::AbstractSerieDao() {}
+
+AbstractSerieDao::~AbstractSerieDao() {}
--- src/data/dao/AbstractSerieDao.h
+++ src/data/dao/AbstractSerieDao.h
@@ -1,6 +1,21 @@
 #ifndef ABSTRACT_SERIE_DAO_H
 #define ABSTRACT_SERIE_DAO_H
 
-// TODO: Implement AbstractSerieDao class
+#include <vector>
+#include "../../model/Serie.h"
+
+using namespace std;
+
+class AbstractSerieDao {
+public:
+    AbstractSerieDao() {}
+    virtual ~AbstractSerieDao() {}
+
+    virtual vector<Serie*>& getSerieList() = 0;
+    virtual Serie* getSerieById(int id) = 0;
+    virtual void add(Serie& serie) = 0;
+    virtual void update(Serie& serie) = 0;
+    virtual void remove(Serie& serie) = 0;
+};
 
 #endif // ABSTRACT_SERIE_DAO_H
