2c16ebef99ebbaa383374b584306de7756ebd7fa
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-05-15 14:10:37 -0300

    Criação do cabeçalho da classe AbstractSerieDao

--- src/data/dao/MemorySerieDao.h
+++ src/data/dao/MemorySerieDao.h
@@ -1,6 +1,23 @@
 #ifndef MEMORY_SERIE_DAO_H
 #define MEMORY_SERIE_DAO_H
 
-// TODO: Implement MemorySerieDao class
+#include "AbstractSerieDao.h"
+#include "../connection/MemoryDBConnection.h"
+
+class MemorySerieDao : public AbstractSerieDao {
+private:
+    MemoryDBConnection* connection;
+    static int lastEntryId;
+
+public:
+    MemorySerieDao();
+    virtual ~MemorySerieDao();
+
+    virtual vector<Serie*>& getSerieList();
+    virtual Serie* getSerieById(int id);
+    virtual void add(Serie& serie);
+    virtual void update(Serie& serie);
+    virtual void remove(Serie& serie);
+};
 
 #endif // MEMORY_SERIE_DAO_H
