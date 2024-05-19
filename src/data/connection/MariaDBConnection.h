41f7390ab483eaeea7c706db62e6962b367c7c65
Luiz Henrique Firmino de Jesus <l176204@dac.unicamp.br>
2024-05-19 00:14:38 -0300

    Implementação do MariaDBConnection

--- src/data/connection/MariaDBConnection.h
+++ src/data/connection/MariaDBConnection.h
@@ -1,6 +1,27 @@
 #ifndef MARIA_DB_CONNECTION_H
 #define MARIA_DB_CONNECTION_H
 
-// TODO: Implement MariaDBConnection class
+#include <string>
+#include "C:\Program Files\MariaDB\MariaDB C++ Connector 64-bit\include\conncpp.hpp"
+
+using namespace std;
+
+class MariaDBConnection
+{
+private:
+    string serverPort = "3306";
+    string serverIp = ""; // TODO: Add your server IP here
+    string serverDatabase = "Si300A2024_06";
+    string serverUser = "Si300A2024_06";
+    string serverPassword = "czn07XWoDJ";
+
+    sql::Driver *driver = nullptr;
+    sql::Connection *connection = nullptr;
+
+public:
+    MariaDBConnection();
+    virtual ~MariaDBConnection();
+    sql::Connection* getConnection() const;
+};
 
 #endif // MARIA_DB_CONNECTION_H
