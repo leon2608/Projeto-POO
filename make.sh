789470f3bd0625974eb8d0b87efffbb097e18d92
Gabriel <gab.ponsoni@gmail.com>
2024-05-24 14:55:30 -0300

    make.sh corrigido

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-g++ -o stvs_project src/Main.cpp src/controller/Controller.cpp src/data/connection/MariaDBConnection.cpp src/data/connection/MemoryDBConnection.cpp src/data/dao/AbstractSerieDao.cpp src/data/dao/MariaDBSerieDao.cpp src/data/dao/MemorySerieDao.cpp src/model/Serie.cpp src/utils/Menu.cpp src/utils/SysInfo.cpp src/utils/Utils.cpp -std=c++11 -lmariadbcpp 
+g++ -o stvs_project src/Main.cpp src/controller/Controller.cpp src/controller/SerieController.cpp src/data/connection/MariaDBConnection.cpp src/data/connection/MemoryDBConnection.cpp src/data/dao/AbstractSerieDao.cpp src/data/dao/MariaDBSerieDao.cpp src/data/dao/MemorySerieDao.cpp src/model/Serie.cpp src/utils/Menu.cpp src/utils/SysInfo.cpp src/utils/Utils.cpp -std=c++11 -lmariadbcpp &&
 ./stvs_project
